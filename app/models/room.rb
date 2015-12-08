1ef063cfd716e56cd8c6731efb1f4f034739a53c
Daniele Pestilli <dpestilli@funnelback.com>
2015-12-08 15:19:17 +0000

    Removed pundit

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ActiveRecord::Base
 	
 	validates_presence_of :price
   # There are 640 station names. Check station_names method in the rooms helper.
-	validates :closest_station, inclusion: { in: (0..639) }
+	#validates :closest_station, inclusion: { in: (0..639) }
 	validates :zone, inclusion: { in: (1..6) }
 	validates :gender, inclusion: { in: %w(none female male) }
 	validates_length_of :description, :maximum => 1000, :allow_blank => true
