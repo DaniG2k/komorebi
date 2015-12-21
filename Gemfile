d4760f5b1f277c7bebcf017face787daff62dada
Daniele Pestilli <dpestilli@funnelback.com>
2015-12-21 16:46:17 +0000

    Fixed uploader

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'mailboxer'
 gem 'chosen-rails'
 gem 'font-awesome-rails'
 gem 'carrierwave', github: 'carrierwaveuploader/carrierwave'
+gem 'mini_magick'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/uploaders/room_images_uploader.rb
+++ app/uploaders/room_images_uploader.rb
@@ -3,7 +3,7 @@ class RoomImagesUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -24,7 +24,7 @@ class RoomImagesUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [400, 400]
+  process :resize_to_limit => [400, 400]
   #
   # def scale(width, height)
   #   # do something
