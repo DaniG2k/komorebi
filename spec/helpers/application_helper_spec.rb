88300000089210da3c52fd55dba302bf4356afdb
Daniele Pestilli <dpestilli@funnelback.com>
2016-01-07 17:01:13 +0000

    Added rspec

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe ApplicationHelper, :type => :helper do
+#  describe "#set_html_lang" do
+#    it 'defaults to english' do
+#    end
+#    it 'changes the html language' do
+#    end
+#  end
+end
